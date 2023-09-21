4dc3a54494fe325b08e3c1989957261444a9576b
warndarkahead <ayush.it.21023@recb.ac.in>
2023-09-21 14:50:16 +0530

    Added Hovering effect

--- script.js
+++ script.js
@@ -9,7 +9,22 @@ const container = document.querySelector(".container");
 //Create a 16x16 grid
 
 for(let i = 0; i <16*16;i++){
-    const div = document.createElement("div");
-    container.appendChild(div);
+    const item = document.createElement("div");
+    item.classList.add("item")
+    container.appendChild(item);
 }
-    
+
+//added hovering effect
+
+const gridItems = document.querySelectorAll(".item");
+
+gridItems.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = "black";
+    });
+
+    item.addEventListener('mouseout', () => {
+        item.style.backgroundColor = "black";
+    });
+});
+
